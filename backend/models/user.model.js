f3fd4610d66f810bed1017147336097ed9119dae
Abdullah <143247953+abdullazmat@users.noreply.github.com>
2024-11-27 16:19:18 +0500

    Mongo DB Models Setup

---
+++ backend/models/user.model.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["student", "recruiter"], // enum is used where we have select some value.
+      required: true,
+    },
+    profile: {
+      bio: {
+        type: String,
+      },
+      skills: [
+        {
+          type: String,
+        },
+      ],
+      resume: {
+        type: String, // URL of the resume
+      },
+      resumeOriginalName: {
+        type: String,
+      },
+      company: {
+        type: mongoose.Schema.Types.ObjectId, // Refers to the Company id
+        ref: "Company",
+      },
+      profilePhoto: {
+        type: String,
+        default: "",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
