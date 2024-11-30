47fafd7f560132cb67eddb0447d59e8b8f5258ae
Abdullah <143247953+abdullazmat@users.noreply.github.com>
2024-11-30 13:10:56 +0500

    Created Navbar Component and FrontEnd Setup

---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,149 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+function Header() {
+  const [user, setUser] = useState(false);
+
+  return (
+    <div>
+      <header className="p-3 mb-3 border-bottom">
+        <div className="container">
+          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <Link
+              to="/"
+              className="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none"
+            >
+              <h3 style={{ fontWeight: "bold" }}>
+                Job{" "}
+                <span style={{ color: "#f83002", fontWeight: "bold" }}>
+                  Hunt
+                </span>
+              </h3>
+            </Link>
+
+            <ul className="nav col-12 col-lg-auto ms-lg-auto me-0 me-lg-5 mb-2 justify-content-center mb-md-0">
+              <li>
+                <Link
+                  to="#"
+                  className="nav-link px-2  text-black nav-links"
+                  style={{ fontWeight: "bold" }}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="#"
+                  className="nav-link px-2 text-black  nav-links"
+                  style={{ fontWeight: "bold" }}
+                >
+                  Jobs
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="#"
+                  className="nav-link px-2 text-black nav-links"
+                  style={{ fontWeight: "bold" }}
+                >
+                  Browse
+                </Link>
+              </li>
+            </ul>
+
+            <div className="dropdown text-end me-0 me-lg-3">
+              {user ? (
+                <Link
+                  to="#"
+                  className="d-block link-body-emphasis text-decoration-none dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <img
+                    src="https://github.com/mdo.png"
+                    alt="mdo"
+                    width="32"
+                    height="32"
+                    className="rounded-circle mt-3 mt-md-0"
+                  />
+                </Link>
+              ) : (
+                <div className="mt-3 mt-lg-0">
+                  <button
+                    className="btn me-3 nav-login-btn "
+                    style={{
+                      backgroundColor: "white",
+                      color: "black",
+                      border: "1px solid black",
+                    }}
+                  >
+                    LogIn
+                  </button>
+                  <button
+                    className="btn nav-signup-btn "
+                    style={{ backgroundColor: "#5F32AD", color: "white" }}
+                  >
+                    Signup
+                  </button>
+                </div>
+              )}
+              <ul className="dropdown-menu text-small">
+                <li>
+                  <div className="d-flex ">
+                    <img
+                      src="https://github.com/mdo.png"
+                      alt="mdo"
+                      width="32"
+                      height="32"
+                      className="rounded-circle  "
+                    />
+                    <p
+                      className="dropdown-item  "
+                      style={{ fontWeight: "bold" }}
+                    >
+                      Abdullah
+                    </p>
+                  </div>
+                </li>
+
+                <li>
+                  <div className="d-flex align-items-center">
+                    <FontAwesomeIcon icon={faUser} className="ms-2" />
+                    <Link
+                      className="dropdown-item text-black view-profile-nav"
+                      to="#"
+                    >
+                      View Profile
+                    </Link>
+                  </div>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <div className="d-flex align-items-center">
+                    <FontAwesomeIcon
+                      icon={faRightFromBracket}
+                      className="ms-2"
+                    />
+                    <Link
+                      className="dropdown-item text-black view-profile-nav"
+                      to="#"
+                    >
+                      Log out
+                    </Link>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import App from "./App.js";
+import "./index.css";
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
